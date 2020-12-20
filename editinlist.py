cb572c7a692c0407be5ab6baad165bfa7b960e38
深渊上的坑 <kkcacat@outlook.com>
2020-12-20 20:46:35 +0800

    add close topSpeed label editor state function

--- editinlist.py
+++ editinlist.py
@@ -8,8 +8,16 @@ class EditInList(QListWidget):
     def __init__(self):
         super(EditInList,self).__init__()
         # click to edit
-        self.clicked.connect(self.item_clicked)  
+        self.clicked.connect(self.item_clicked)
 
+    def mouseDoubleClickEvent(self, event):
+        # close edit
+        self.closePersistentEditor(self.currentItem())
+
+    def leaveEvent(self, event):
+        # close edit  
+        self.closePersistentEditor(self.currentItem())    
+        
     def item_clicked(self, modelindex: QModelIndex) -> None:
         self.edited_item = self.currentItem()
         self.closePersistentEditor(self.edited_item)
