fa67e08045f585be6cdd33cf38739738fdea58ff
深渊上的坑 <kkcacat@outlook.com>
2021-02-01 23:29:41 +0800

    Auto stash before merge of "canvas" and "origin/canvas"

--- libs/editinlist.py
+++ libs/editinlist.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import sys, time
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
@@ -28,4 +29,36 @@ class EditInList(QListWidget):
     def leaveEvent(self, event):
         # close edit
         for i in range(self.count()):
+=======
+import sys, time
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class EditInList(QListWidget):
+    def __init__(self):
+        super(EditInList,self).__init__()
+        # click to edit
+        self.clicked.connect(self.item_clicked)  
+
+    def item_clicked(self, modelindex: QModelIndex) -> None:
+        self.edited_item = self.currentItem()
+        self.closePersistentEditor(self.edited_item)
+        item = self.item(modelindex.row())
+        # time.sleep(0.2)
+        self.edited_item = item
+        self.openPersistentEditor(item)
+        # time.sleep(0.2)
+        self.editItem(item)
+
+    def mouseDoubleClickEvent(self, event):
+        # close edit
+        for i in range(self.count()):
+            self.closePersistentEditor(self.item(i))
+
+    def leaveEvent(self, event):
+        # close edit
+        for i in range(self.count()):
+>>>>>>> Stashed changes
             self.closePersistentEditor(self.item(i))
